1518b15d25e506a22a0c2df7df8bd018eff89454
jbe <jbe@localhost>
2017-09-18 01:44:32 +0200

    Added "verification" table to store verification requests

--- core.sql
+++ core.sql
@@ -207,6 +207,38 @@ COMMENT ON COLUMN "member_history"."id"    IS 'Primary key, which can be used to
 COMMENT ON COLUMN "member_history"."until" IS 'Timestamp until the data was valid';
 
 
+CREATE TABLE "verification" (
+        "id"                    SERIAL8         PRIMARY KEY,
+        "requested"             TIMESTAMPTZ,
+        "request_origin"        JSONB,
+        "request_data"          JSONB,
+        "verified"              TIMESTAMPTZ,
+        "verification_origin"   JSONB,
+        "verification_data"     JSONB,
+        "denied"                TIMESTAMPTZ,
+        "member_id"             INT4            REFERENCES "member" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+        "comment"               TEXT,
+        CONSTRAINT "verified_and_denied_conflict" CHECK (
+          "verified" ISNULL OR "denied" ISNULL ) );
+CREATE INDEX "verification_requested_idx" ON "verification" ("requested");
+CREATE INDEX "verification_open_request_idx" ON "verification" ("requested") WHERE "verified" ISNULL AND "denied" ISNULL;
+CREATE INDEX "verification_verified_idx" ON "verification" ("verified");
+CREATE INDEX "verification_denied_idx" ON "verification" ("denied");
+CREATE INDEX "verification_member_id_idx" ON "verification" ("member_id");
+
+COMMENT ON TABLE "verification" IS 'Request to verify a participant';
+
+COMMENT ON COLUMN "verification"."requested" IS 'Timestamp when request for verification has been submitted';
+COMMENT ON COLUMN "verification"."request_origin" IS 'JSON data containing information about the origin of the request (e.g. IP address or hostname)';
+COMMENT ON COLUMN "verification"."request_data" IS 'JSON data containing information about the entity to be verified (e.g. real name, address, etc.)';
+COMMENT ON COLUMN "verification"."verified" IS 'Timestamp when request for verification has been accepted by authority';
+COMMENT ON COLUMN "verification"."verification_origin" IS 'JSON data containing information about the authority or operator who accepted or denied the request';
+COMMENT ON COLUMN "verification"."verification_data" IS 'JSON data containing additional verified data, but all public information shall be copied to "member"."identification", "member"."verification" and/or "member"."name" if applicable for setup';
+COMMENT ON COLUMN "verification"."denied" IS 'Timestamp when request for verification has been denied by authority';
+COMMENT ON COLUMN "verification"."member_id" IS 'Timestamp when request for verification has been denied by authority';
+COMMENT ON COLUMN "verification"."comment" IS 'Administrative comment';
+
+
 CREATE TABLE "member_settings" (
         "member_id"             INT4            PRIMARY KEY REFERENCES "member" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
         "settings"              JSONB           NOT NULL CHECK (jsonb_typeof("settings") = 'object') );
@@ -6336,6 +6368,7 @@ CREATE FUNCTION "delete_private_data"()
         "password_reset_secret_expiry" = NULL,
         "location"                     = NULL;
       -- "text_search_data" is updated by triggers
+      DELETE FROM "verification";
       DELETE FROM "member_settings";
       DELETE FROM "member_useterms";
       DELETE FROM "member_profile";
